df467b02475803640b1653743219267d895d3138
kawarimidoll <kawarimidoll+git@gmail.com>
2024-07-17 15:27:38 +1000

    feat: use url args

--- testapp/views.py
+++ testapp/views.py
@@ -8,8 +8,12 @@ today = datetime.today()
 @app.route("/")
 def index():
     data_dict = {
-            'date': today.strftime("%Y-%m-%d"),
-            'pages': [ 'about', 'page1', 'page2' ]
+        "date": today.strftime("%Y-%m-%d"),
+        "pages": [
+            {"name": "about", "url": "/about"},
+            {"name": "page1", "url": "/page/1"},
+            {"name": "page2", "url": "/page/2"},
+        ],
     }
     return render_template("testapp/index.html", data_dict=data_dict)
 
@@ -18,10 +22,8 @@ def index():
 def about():
     return render_template("testapp/about.html")
 
-@app.route("/page1")
-def page1():
-    return "This is page1"
 
-@app.route("/page2")
-def page2():
-    return "This is page2"
+@app.route("/page/<page_num>")
+def page(page_num):
+    data_dict = {"page_num": int(page_num)}
+    return render_template("testapp/page.html", data_dict=data_dict)
