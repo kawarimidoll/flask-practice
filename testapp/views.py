77e46af8d8ea08301fdb9c57116d99823f020b2a
kawarimidoll <kawarimidoll+git@gmail.com>
2024-07-17 10:19:48 +1000

    feat: use loop in template

--- testapp/views.py
+++ testapp/views.py
@@ -7,9 +7,21 @@ today = datetime.today()
 
 @app.route("/")
 def index():
-    return render_template("testapp/index.html", date_string=today.strftime("%Y-%m-%d"))
+    data_dict = {
+            'date': today.strftime("%Y-%m-%d"),
+            'pages': [ 'about', 'page1', 'page2' ]
+    }
+    return render_template("testapp/index.html", data_dict=data_dict)
 
 
 @app.route("/about")
 def about():
     return "This is about page"
+
+@app.route("/page1")
+def page1():
+    return "This is page1"
+
+@app.route("/page2")
+def page2():
+    return "This is page2"
