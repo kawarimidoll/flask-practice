f61a8b5830d728d5514d73eac00907a1b0e441df
kawarimidoll <kawarimidoll+git@gmail.com>
2024-07-18 20:36:45 +1000

    refactor: improve structure

---
+++ bomcal/logic.py
@@ -0,0 +1,119 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+
+# url = "https://www.accuweather.com/en/au/melbourne/26216/daily-weather-forecast/26216"
+
+
+def fetch_bom(state, city):
+    url = f"http://www.bom.gov.au/{state}/forecasts/{city[0]}.shtml"
+    # print(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(url, headers=headers)
+    # print(response)
+    return response
+
+
+def extract_data(raw_html):
+    soup = BeautifulSoup(raw_html, "html.parser")
+    # print(soup.prettify())
+
+    days = soup.find_all("div", {"class": "day"})
+    today = datetime.today()
+    result = []
+
+    for idx, d in enumerate(days):
+        summary_list = []
+
+        summary_list.append(d.find("dd", {"class": "summary"}).text)
+
+        maxElm = d.find("em", {"class": "max"})
+        minElm = d.find("em", {"class": "min"})
+
+        if maxElm is not None and minElm is not None:
+            summary_list.append(f"{maxElm.text}/{minElm.text}")
+
+        summary_list.append(d.find("em", {"class": "pop"}).text.strip())
+
+        description = d.find("p").text
+
+        # print(" ".join(summary_list))
+        # print(description)
+        # print()
+
+        current_date = (today + timedelta(days=idx)).strftime("%Y%m%d")
+        next_date = (today + timedelta(days=idx + 1)).strftime("%Y%m%d")
+
+        result.append(
+            {
+                "current_date": current_date,
+                "next_date": next_date,
+                "summary": " ".join(summary_list),
+                "description": description,
+            }
+        )
+
+    return result
+
+
+# TODO: when another state is added, timezone should be changed
+ICAL_TEMPLATE = """BEGIN:VCALENDAR
+PRODID:Bomcal
+VERSION:2.0
+METHOD:PUBLISH
+CALSCALE:GREGORIAN
+X-WR-CALNAME:iCal Weather in week (#STATE_NAME#/#CITY_NAME#)
+X-WR-CALDESC:iCal formatted BOM Forecast
+X-WR-TIMEZONE:Australia/Melbourne
+#VEVENT#
+BEGIN:VTIMEZONE
+TZID:Australia/Melbourne
+BEGIN:STANDARD
+DTSTART:19700101T000000
+TZOFFSETFROM:+1000
+TZOFFSETTO:+1000
+END:STANDARD
+END:VTIMEZONE
+END:VCALENDAR"""
+
+VEVENT_TEMPLATE = """BEGIN:VEVENT
+UID:bomcal/#STATE_PATH#/#CITY_PATH#/#CURRENT_DATE#
+DESCRIPTION:#DESCRIPTION#
+DTSTART:#CURRENT_DATE#
+DTEND:#NEXT_DATE#
+SUMMARY:#SUMMARY#
+END:VEVENT"""
+
+
+def gen_ical_string(weather_data_dict_list, state, city):
+    # weather_data_dict_list example
+    # [{'current_date': '20240718', 'next_date': '20240719', 'summary': 'Cloudy. 20%', 'description': 'Cloudy...'}, ]
+    vevent_list = []
+    for item in weather_data_dict_list:
+        vevent = (
+            VEVENT_TEMPLATE.replace("#CURRENT_DATE#", item["current_date"])
+            .replace("#NEXT_DATE#", item["next_date"])
+            .replace("#SUMMARY#", item["summary"])
+            .replace("#DESCRIPTION#", item["description"])
+        )
+        vevent_list.append(vevent)
+
+    return (
+        ICAL_TEMPLATE.replace("#VEVENT#", "\n".join(vevent_list))
+        .replace("#STATE_NAME#", state.upper())
+        .replace("#STATE_PATH#", state.lower())
+        .replace("#CITY_PATH#", city[0])
+        .replace("#CITY_NAME#", city[1])
+    )
+
+
+# e.g.
+# state: vic
+# city: [ "phillipisland", "Phillip Island" ]
+def get_forecast(state, city):
+    response = fetch_bom(state, city)
+    extracted_data = extract_data(response.text)
+    return gen_ical_string(extracted_data, state, city)
--- bomcal/views.py
+++ bomcal/views.py
@@ -1,67 +1,39 @@
-from flask import render_template, abort
+from flask import render_template, abort, make_response
 from bomcal import app
-from bomcal.logic.get_forecast import get_forecast
+from bomcal.logic import get_forecast
 
 
 # STATES = ["nsw", "vic", "qld", "wa", "sa", "tas", "nt"]
 
-
+# http://www.bom.gov.au/vic/forecasts/towns.shtml
 VIC_CITIES = [
-    "melbourne",
-    "geelong",
-    "ballarat",
-    "avalon",
-    "laverton",
-    "tullamarine",
-    "moorabbin",
-    "scoresby",
-    "watsonia",
-    "mount",
-    "yarra",
-    "frankston",
-    "cranbourne",
-    "mornington",
-    "cerberus",
-    "phillip",
-    "rhyll",
-    "dandenong",
-    "aireys",
-    "wonthaggi",
-    "gisborne",
-    "kyneton",
-    "melton",
-    "pakenham",
-    "sunbury",
-    "torquay",
+    [ "melbourne", "Melbourne" ],
+    [ "geelong", "Geelong" ],
+    [ "ballarat", "Ballarat" ],
+    [ "avalon", "Avalon" ],
+    [ "laverton", "Laverton" ],
+    [ "tullamarine", "Tullamarine" ],
+    [ "moorabbin", "Moorabbin" ],
+    [ "scoresby", "Scoresby" ],
+    [ "watsonia", "Watsonia" ],
+    [ "mountdandenong", "Mount Dandenong" ],
+    [ "yarraglen", "Yarra Glen" ],
+    [ "frankston", "Frankston" ],
+    [ "cranbourne", "Cranbourne" ],
+    [ "mornington", "Mornington" ],
+    [ "cerberus", "Cerberus" ],
+    [ "phillipisland", "Phillip Island" ],
+    [ "rhyll", "Rhyll" ],
+    [ "dandenong", "Dandenong" ],
+    [ "aireysinlet", "Airey's Inlet" ],
+    [ "wonthaggi", "Wonthaggi" ],
+    [ "gisborne", "Gisborne" ],
+    [ "kyneton", "Kyneton" ],
+    [ "melton", "Melton" ],
+    [ "pakenham", "Pakenham" ],
+    [ "sunbury", "Sunbury" ],
+    [ "torquay", "Torquay" ],
 ]
-
-# TODO: when another state is added, timezone should be changed
-ICAL_TEMPLATE = """BEGIN:VCALENDAR
-PRODID:Bomcal
-VERSION:2.0
-METHOD:PUBLISH
-CALSCALE:GREGORIAN
-X-WR-CALNAME:iCal Weather in week (#STATE#/#CITY#)
-X-WR-CALDESC:iCal formatted BOM Forecast
-X-WR-TIMEZONE:Australia/Melbourne
-#VEVENT#
-BEGIN:VTIMEZONE
-TZID:Australia/Melbourne
-BEGIN:STANDARD
-DTSTART:19700101T000000
-TZOFFSETFROM:+1000
-TZOFFSETTO:+1000
-END:STANDARD
-END:VTIMEZONE
-END:VCALENDAR"""
-
-VEVENT_TEMPLATE = """BEGIN:VEVENT
-UID:bomcal/#STATE#/#CITY#/#CURRENT_DATE#
-DESCRIPTION:#DESCRIPTION#
-DTSTART:#CURRENT_DATE#
-DTEND:#NEXT_DATE#
-SUMMARY:#SUMMARY#
-END:VEVENT"""
 
 
 @app.route("/")
@@ -72,27 +44,13 @@ def index():
 
 @app.route("/<state>/<city>.ics")
 def cal(state=None, city=None):
-    if state != "vic" or city not in VIC_CITIES:
+    # remove non-alphanumeric characters
+    city_data = next((item for item in VIC_CITIES if item[0] == city), None)
+    if state != "vic" or city_data is None:
         abort(404)
 
-    result = get_forecast(state, city)
-    # result example
-    # [{'current_date': '20240718', 'next_date': '20240719', 'summary': 'Cloudy. 20%', 'description': 'Cloudy...'}, ]
-    vevent_list = []
-    for item in result:
-        vevent = (
-            VEVENT_TEMPLATE.replace("#CURRENT_DATE#", item["current_date"])
-            .replace("#NEXT_DATE#", item["next_date"])
-            .replace("#SUMMARY#", item["summary"])
-            .replace("#DESCRIPTION#", item["description"])
-        )
-        vevent_list.append(vevent)
-
-    ical_string = (
-        ICAL_TEMPLATE.replace("#VEVENT#", "\n".join(vevent_list))
-        .replace("#STATE#", state)
-        .replace("#CITY#", city)
-    )
-
-    print(ical_string)
-    return "ics file!"
+    ical_string = get_forecast(state, city_data)
+    # print(ical_string)
+    response = make_response(ical_string)
+    response.headers["content-type"] = "text/calendar"
+    return response
